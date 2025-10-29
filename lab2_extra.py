a9f3cc7891938054232ec7d182916699cf430c80
Александр Шалягин <sashka@MacBook-Pro-Aleksandr.local>
2025-10-29 19:30:27 +0300

    extra task correct version

--- lab2_extra.py
+++ lab2_extra.py
@@ -1,5 +1,6 @@
 import csv
 
+
 def finder(book_file):
     reader = csv.DictReader(book_file, delimiter=";")
     publishing = []
@@ -10,18 +11,19 @@ def finder(book_file):
             publishing.append(row["Publisher"])
     return publishing
 
-def populad_books(book_file):
+
+def popular_books(book_file):
     book_file.seek(0)
     reader = csv.DictReader(book_file, delimiter=";")
     books = []
     for row in reader:
-        books.append((row["Book-Title"], row["Downloads"]))
-    books.sort(key= lambda x: x[1], reverse=True)
+        books.append((row["Book-Title"], int(row["Downloads"])))
+    books.sort(key=lambda x: x[1], reverse=True)
     return books[:20]
 
 with open("books-en.csv", encoding="latin-1") as book_file:
     print(*finder(book_file), sep=", ")
     print("\n20 The most popular books: \n")
-    for i, (title, downloads) in enumerate(populad_books(book_file), 1):
+    for i, (title, downloads) in enumerate(popular_books(book_file), 1):
         print(f"{i}. {title} - {downloads} downloads")
 
